73d1d13a2f035e4e9896d669e3a030268833ac95
005abhi <abhinandan111003@gmai.com>
2025-05-23 22:26:02 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+
+dotenv.config({ path: "./config/config.env" }); // ✅ Add this
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
